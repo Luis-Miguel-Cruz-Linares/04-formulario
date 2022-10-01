4c4974add3a6fcade0269ead3a3ae40c9b7b15bd
Luis-Miguel-Cruz-Linares <a3521110400@alumno.uttehuacan.edu.mx>
2022-09-30 22:27:33 -0500

    Primera versión de 04-formuario

---
+++ src/components/Formulario.vue
@@ -0,0 +1,74 @@
+<script>
+    export default {
+        data: () => ({
+            proyecto: "",
+            tipo: "",
+            urgente: false,
+            proyectos: [],
+        }),
+        methods: {
+            registrarProyecto() {
+                const proyecto = {
+                    proyecto: this.proyecto,
+                    tipo:this.tipo,
+                    urgente: this.urgente,
+                };
+                this.proyectos.push(proyecto);
+                
+                this.proyectos = "";
+                this.tipo = "";
+                this.urgente = false;
+            },
+        },
+    };
+</script>
+
+<template>
+    <form @submit.prevent="registrarProyecto">
+            <div class="mb-3">
+                <label class="form-label">Proyecto</label>
+                <input v-model.trim="proyecto" type="text" class="form-control" required/>
+            </div>
+
+            <div class="mb-3">
+                <label class="form-label">Actividad</label>
+                <select v-model.trim="tipo" class="form-select" required>
+                    <option disabled selected value="">Selecciona un tipo de actividad</option>
+                    <option>Aplicaciones Web con Vue.js</option>
+                    <option>Backend Services con Node.js</option>
+                    <option>App móvil con React Native</option>
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-check-label">Urgente</label>
+                <input v-model="urgente" type="checkbox" class="form-check-control" />
+            </div>
+          
+            <button type="submit" class="btn btn-primary">Guardar</button>
+      </form>
+      <hr>
+
+      <div class="table-responsive">
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Proyecto</th>
+                    <th>Tipo</th>
+                    <th>Urgente</th>
+                </tr>
+
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Laravel app</td>
+                        <td>Laravel</td>
+                        <td class="bg-danger">Sí / No</td>
+                    </tr>
+                </tbody>
+            </thead>
+        </table>
+      </div>
+      
+</template>
